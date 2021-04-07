18f94c0b35c7a278fd1d8df37b3503fa8f288670
Harry-Bro <likeharrybro@gmail.com>
2021-04-07 21:55:33 +0900

    chore: Initialize project

---
+++ src/main/java/com/harrybro/email/springemailauthentication/SpringEmailAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.harrybro.email.springemailauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEmailAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEmailAuthenticationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/harrybro/email/springemailauthentication/SpringEmailAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harrybro.email.springemailauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEmailAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
